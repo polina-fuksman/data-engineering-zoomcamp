23eacd2a1aa56c3ed63cce55559671843f4707ab
Polina Fuksman <spilt-deeds-2@icloud.com>
2025-02-24 18:41:47 +0000

    fhv_tripdata

--- homework/homework_4/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
+++ homework/homework_4/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
@@ -1,17 +1,25 @@
 {{ config(materialized='table') }}
 
 with trips_data as (
-    select * from {{ ref('fact_trips') }}
-)
-    select 
-    -- Revenue grouping 
-    {{ dbt.date_trunc("year", "pickup_datetime") }} as revenue_year, 
-    {{ dbt.date_trunc("quarter", "pickup_datetime") }} as revenue_quarter,
+    select
+    service_type,
+    extract(year from pickup_datetime) as r_year,
+    extract(quarter from pickup_datetime) as r_quarter,
+    sum(total_amount) as revenue,
+    
+    from `taxi-rides-ny-448612`.`hw4_2019_2020`.`fact_trips`
 
-
-    -- Revenue calculation 
-    sum(total_amount) as revenue_monthly_total_amount,
-    revenue_monthly_total_amount - LAG(revenue_monthly_total_amount, offset 4) OVER(ORDER BY revenue_year,revenue_quarter) AS change
-
-    from trips_data
-    group by 1,2
+    where extract(year from pickup_datetime) between 2019 and 2020
+    group by
+    service_type,
+    r_year,
+    r_quarter
+    )
+select 
+service_type,
+r_year, 
+r_quarter,
+revenue,
+revenue / LAG(revenue,4) OVER (partition by service_type order by service_type, r_year, r_quarter)*100 as changes
+from trips_data
+order by service_type desc
